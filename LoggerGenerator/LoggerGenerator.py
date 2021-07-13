e28d008bca795ab045f0b78cc1744a44d435c2bc
Akasan <daisuke1024akagawa@gmail.com>
2021-07-13 13:47:06 +0900

    Fix: setting folder and file name

--- LoggerGenerator/LoggerGenerator.py
+++ LoggerGenerator/LoggerGenerator.py
@@ -155,6 +155,9 @@ class LoggerGenerator:
         if self._FILENAME is None:
             self._FILENAME = f"{self._LOG_FOLDER}{year}{month}{day}-{hour}{minute}{second}_{self._split_cnt}.log"
 
+        else:
+            self._FILENAME = f"{self._LOG_FOLDER}{self._FILENAME}"
+
         self._log = self._get_log()
 
     def _get_log(self) -> logging.Logger:
