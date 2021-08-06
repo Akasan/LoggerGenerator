8ebed0b59885c71daf8c5bbb74a6cebd12ef8a3a
Akasan <daisuke1024akagawa@gmail.com>
2021-08-06 14:29:37 +0900

    Update version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def _get_requires_from_file(filename):
 
 setup(
     name="LoggerGenerator",
-    version="1.2.0",
+    version="1.2.1",
     license="MIT",
     author="Akagawa Daisuke",
     url="http://github.com/Akasan/LoggerGenerator",
